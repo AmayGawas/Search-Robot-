fe69a355bc6a4b96fb665443d611b681384dc011
AmayGawas <amaygawas07@gmail.com>
2023-06-13 23:26:23 +0530

    added searchbox

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Cardlist from "./cardlist";
+import Searchbox from "./searchbox";
+
 const App =()=>{
     return(
+        <div className="tc">
+
+        <h1>Search for Robot</h1>
+         <Searchbox/>
         <Cardlist/>
+        </div>
     );
 }
 export default App;
---
+++ src/searchbox.js
@@ -0,0 +1,8 @@
+const Searchbox=()=>{
+    return(
+        <div className="pa2">
+            <input className="tc pa3 ba b--green bg-lightest-blue" type="search" name="" id="" placeholder="Search here" />
+        </div>
+    );
+}
+export default Searchbox
