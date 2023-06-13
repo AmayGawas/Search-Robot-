1dc340a52f7a89d0bcdb1ddf0a0a5715c5087201
AmayGawas <amaygawas07@gmail.com>
2023-06-13 21:54:39 +0530

    Added and styled cards

---
+++ src/robot.js
@@ -0,0 +1,58 @@
+export const robots=[
+    {
+        id:1,
+        name: 'Arran',
+        username: 'arrforce',
+        email: 'arran@des.com'
+    },
+    {
+        id:2,
+        name: 'Zeke',
+        username: 'zeke4u',
+        email: 'zeke@des.com'
+    },
+    {
+        id:3,
+        name: 'Ereh',
+        username: 'thefounder',
+        email: 'ereh@des.com'
+    },
+
+    {
+        id:4,
+        name: 'Armin',
+        username: 'colosull',
+        email: 'armin@des.com'
+    },
+    {
+        id:5,
+        name: 'Sasha',
+        username: 'sashhogger',
+        email: 'sasha@des.com'
+    },
+    {
+        id:6,
+        name: 'Jean',
+        username: 'jean',
+        email: 'jean@des.com'
+    },
+    {
+        id:7,
+        name: 'LeviAck',
+        username: 'ackerman',
+        email: 'levi@des.com'
+    },
+    {
+        id:8,
+        name: 'Berthold',
+        username: 'formercol',
+        email: 'berth@des.com'
+    },
+    {
+        id:9,
+        name: 'Reiner',
+        username: 'armouredtitan',
+        email: 'reiner@des.com'
+    },
+
+]
